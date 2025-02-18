d0998efc13c111c0117d6a373820caa0ff2873d6
valeevaldez10 <valentina.valdez.v@ucb.edu.bo>
2025-02-18 08:49:09 -0400

    avance

--- clase 18-2-25.R
+++ clase 18-2-25.R
@@ -5,4 +5,23 @@ library(udpipe) #etiquetado
 library(hunspell) #ortografía
 library(syuzhet) #análisis de sentimiento
 
+#configuración para caracteres e idioma
+options(stringsAsFactors=FALSE)
+Sys.setlocale(category = "LC_ALL",locale="es_ES.UTF-8")
+##############
 load("datasets/larazon.RData")
+##############
+#Ortografía
+list_dictionaries()
+texto<-"hola pequeña vandida"
+aux<-hunspell(texto,dict = "es_ES")
+hunspell_suggest(aux[[1]],dict = "es_ES") #nos dice cuales pueden ser posibles correcciones
+#caso archivo la razon
+bd$titular
+hunspell(bd$titular[1:5],dict = "es_ES")
+
+clarazon <- VCorpus(VectorSource(bd$titular))
+clarazon[[20]] #toma en cuenta a cada titular como un documento
+clarazon[[20]]$content
+
+
