0ac7aa464cfdf42a42bea77d648efca671b32d74
valeevaldez10 <valentina.valdez.v@ucb.edu.bo>
2025-02-25 08:43:53 -0400

    clase 25-02-25

--- clase 20-2-25.R
+++ clase 20-2-25.R
@@ -9,7 +9,7 @@ library(stringr)
 library(pdftools)
 library(ggplot2)
 library(wordcloud2)
-library(ggworldcloud)
+library(ggwordcloud)
 library(igraph)
 library(visNetwork)
 
@@ -76,12 +76,73 @@ aux<-tdm_V %>% as.matrix() %>% rowSums() %>%
 aux$words<-rownames(aux)
 ggplot(aux %>% filter(freq>2),aes(freq,word))+geom_point()
 wordcloud2(aux[,c(2,1)]) #el orden es la palabra y la frecuencia para que wordcloud pueda leer el documento
-ggplot(aux,aes(label=words,size)
 
-
-       
 #para mis pdfs
 aux1<-tdm_pdf %>% as.matrix() %>% rowSums() %>% 
   data.frame(freq=.)
 aux1$words<-rownames(aux1)
 wordcloud2(aux1[,c(2,1)])
+
+#CLASE 25/02/2024
+ggplot(aux,aes(label=words,size=freq))+
+  geom_text_wordcloud(col="darkblue")+
+  scale_size_area(max_size = 15)+
+  theme_minimal()
+
+### Análisis descriptivo: sentimiento
+auxs<-get_nrc_sentiment(aux$words,language = "spanish")
+rownames(auxs)<-aux$words
+
+#gráfico de barras con las frecuencias de cada sentimiento
+barplot(apply(auxs,2,sum),horiz=T,las=1)
+# creamos una BD pero incluimos las frecuencias como
+# palabras de nuestro dataframe original
+auxsc<-auxs %>% mutate(words=aux$words,freq=aux$freq)
+ggplot(auxsc,aes(label=words,size=freq,col=fear))+
+  geom_text_wordcloud()+
+  theme_minimal()
+#Debido a que es una revista con noticias de economía
+auxsc<-auxsc %>% filter(words!="economía")
+wordcloud2(auxsc %>% select(words,freq),
+           color =ifelse(auxsc$fear==1,"red","black"))
+
+### Análisis diagnóstico: asociación 
+#palabras que participan con la palabra arce, tienen correlación y un valor de corr min
+findAssocs(tdm_V,"arce",0.2) 
+findAssocs(tdm_V,"ministro",0.1) 
+#se puede hacer un análisis de sentimiento con las palabras que se correlacionan
+#con ministro en mínimo un 10%
+
+#Análisis diagnóstico: redes
+mxd<-as.matrix(dtm_V) %*% t(as.matrix(dtm_V))
+mxt<-as.matrix(tdm_V) %*% t(as.matrix(tdm_V))
+dim(mxd)
+dim(mxt)
+
+dim(tdm_V)
+#se lo reduce porque ocupa mucha memoria
+tdm_reducido <- removeSparseTerms(tdm_V,sparse = 0.99) #remueve palabras que no tienen mucha participación en el 99%
+dim(tdm_reducido)
+mxt<-as.matrix(tdm_reducido) %*% t(as.matrix(tdm_reducido))
+
+#Recomendaciones: reducir la cantidad de terminos con base a las frecuencias
+g <-graph.adjacency(mxt,weighted = T,mode = "undirected")
+g<-simplify(g)
+V(g)$label<-V(g)$name
+V(g)$degree<-degree(g)
+data<-toVisNetworkData(g)
+nodes = data$nodes;edges=data$edges
+set.seed(12345)
+grupos<-cluster_label_prop(g)
+nodes$value<-prop.table(nodes$degree)*1000
+nodes$group<-grupos$membership
+visNetwork(nodes,edges)
+visNetwork(nodes,edges) %>% visPhysics(enabled = FALSE)
+### Análisis diagnóstico: agrupamiento
+#según esto podemos ver que tan distintos son los documentos entre ellos
+#esta clasificación se da dadas las palabras que se están usando
+kmeans(as.matrix(dtm_pdf),3)
+res<-kmeans(as.matrix(dtm_pdf),3)
+data.frame(res$cluster)
+
+
