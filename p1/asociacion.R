328c8a3778d5ecc899626d1ccff921e11ef464f0
John Doe <valentina.valdez.v@ucb.edu.bo>
2025-03-17 13:26:05 -0400

    practica

--- p1/asociacion.R
+++ p1/asociacion.R
@@ -11,14 +11,32 @@ dfp<-read.csv("datasets/BreadBasket_DMS.csv")
 dfp %>% filter(Item=="NONE")
 dfp<-dfp %>% select(-c(1:2)) %>% filter(Item!="NONE")
 dfp$Transaction<-paste0("T",dfp$Transaction)
-##dfp1<-dfp[!duplicated(dfp), ]
+#dfp1<-dfp[!duplicated(dfp), ]
 bdp<-as(split(dfp$Item,dfp$Transaction),"transactions")
 bdp
-##bdp1<-as(split(dfp1$Item,dfp1$Transaction),"transactions")
-##bdp1
+#bdp1<-as(split(dfp1$Item,dfp1$Transaction),"transactions")
+#bdp1
 
 #para el comercio electrónico
+dfc <- read.csv("datasets/ecommerce.csv")
+dfc <- dfc %>%
+  filter(!is.na(Description), !is.na(InvoiceNo)) %>%
+  distinct(InvoiceNo, Description)
+dfa<-dfc
+dfa$InvoiceNo <- as.character(dfa$InvoiceNo)
+dfa<- dfa[grepl("^[0-9]+$", dfa$InvoiceNo), ]
+dfa$InvoiceNo <- as.integer(dfa$InvoiceNo)
+dfa$InvoiceNo <- order(dfa$InvoiceNo,decreasing = FALSE)
+dfa$InvoiceNo<-paste0("T",dfa$InvoiceNo)
+bda<-as(split(dfa$Description,dfa$InvoiceNo),"transactions")
+bda
 
+
+#531040 filas
+#dfc$InvoiceNo <- order(dfc$InvoiceNo,decreasing = FALSE)
+#dfc$InvoiceNo<-paste0("T",dfc$InvoiceNo)
+#bdc <- as(split(dfc$Description,dfc$InvoiceNo),"transactions")
+#bdc
 
 
 
